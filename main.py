c80fbe15b32ddab6556d95b5fe737e22736e8a11
0msk <snarrpy@gmail.com>
2024-04-05 13:15:17 +0300

    Modified main.py. Added functionality to individual modules

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from aiogram import Bot, Dispatcher
+from handlers import user_handlers
+
 from config_data.config import Config, load_config
 
 # Загружаем конфиг в переменную config
@@ -8,5 +10,8 @@ config: Config = load_config()
 bot = Bot(token=config.tg_bot.token)
 dp = Dispatcher()
 
+# Регистриуем роутеры в диспетчере
+dp.include_router(user_handlers.router)
 
+# Запускаем polling
 dp.run_polling(bot)
